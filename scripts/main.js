e5ce8753867ef9f318058b187fc0f86a1ed5a84c
Rast999 <adimitrascu94@gmail.com>
2019-01-30 19:58:34 +0200

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    var navBar = $('#nav-top').eq(0);
+    navBar.attr('navHeight', navBar.height());
+    $(window).on("scroll", changeNavBackground);
+})
+
+function changeNavBackground(event) {
+    var windowWidth = $(window).width();
+    var headerImg = $('#header-img').eq(0);
+    var brandImg = $('img.logo').eq(0);
+    var navBar = $('#nav-top').eq(0);
+    var triggerHeight = headerImg.height() - navBar.attr('navHeight');
+    console.log(event.pageY);
+    if (event.originalEvent.pageY >= triggerHeight) {
+        if (!navBar.hasClass('bg-dark')) {
+            navBar.removeClass('bg-semitransparent navbar-light');
+            navBar.addClass('bg-dark navbar-dark');
+            // if window width less than 576 don't animate/change height, it affects menu collapsing
+            if (windowWidth >= 576) { 
+                navBar.stop().animate(
+                    {height: "40px"},
+                    700
+                );
+                brandImg.stop().animate(
+                    {height: "35px",
+                        width: "35px"},
+                    700
+                );
+            }
+        }
+    } else if (navBar.hasClass('bg-dark')) {
+            navBar.removeClass('bg-dark navbar-dark');
+            navBar.addClass('bg-semitransparent navbar-light');
+            navBar.stop().animate(
+                {height: navBar.attr('navHeight') + "px"},
+                700,
+                function(){
+                    navBar.removeAttr('style');
+                }
+            );
+            brandImg.stop().animate(
+                {height: "50px",
+                    width: "50px"},
+                700,
+                function(){
+                    navBar.removeAttr('style');
+                }
+            );
+    }
+}
